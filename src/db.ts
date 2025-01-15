18ced431888f19b29020888f49a0d5deae72267a
developedbygeo <74512500+developedbygeo@users.noreply.github.com>
2025-01-16 00:31:48 +0200

    feat: implement singleton pattern for Neo4jConnection and simplify session execution

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,24 @@
 import { Driver, Session, auth, driver } from 'neo4j-driver';
 
 export class Neo4jConnection {
+    private static instance: Neo4jConnection | null = null;
+
     driver: Driver;
 
     constructor(uri: string, user: string, password: string) {
         this.driver = driver(uri, auth.basic(user, password));
+    }
+
+    // Get the singleton instance
+    public static getInstance(
+        uri: string,
+        user: string,
+        password: string
+    ): Neo4jConnection {
+        if (!Neo4jConnection.instance) {
+            Neo4jConnection.instance = new Neo4jConnection(uri, user, password);
+        }
+        return Neo4jConnection.instance;
     }
 
     close(): void {
@@ -17,7 +31,7 @@ export class Neo4jConnection {
     ): Promise<void> {
         const session: Session = this.driver.session();
         try {
-            await session.executeWrite((tx) => tx.run(query, parameters));
+            await session.run(query, parameters);
         } finally {
             await session.close();
         }
